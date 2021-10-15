d233119d2f5ad3d15066a4e6f1c82988ac75f51e
sahil161102 <85942526+sahil161102@users.noreply.github.com>
2021-10-15 19:01:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,15 +9,10 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-var image, car1_img, car2_img, car3_img, car4_img;
+var image;
 
 function preload(){
   track = loadImage("../images/quiz.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
 }
 
 function setup(){
